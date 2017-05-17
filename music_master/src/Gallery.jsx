dc5e118359de81762022e6234a62804ae5ed32fa
Lakshmi Kanth <a.c.lakshmikanth@gmail.com>
2017-05-17 09:15:30 +0530

    Added play/pause buttons for artist audio files

--- music_master/src/Gallery.jsx
+++ music_master/src/Gallery.jsx
@@ -1,6 +1,44 @@
 import React, { Component } from 'react';
 
 class Gallery extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			playingUrl: '',
+			playiing: false,
+			audio: null
+		};
+	}
+	
+	playAudio(audioUrl) {
+		let audio = new Audio(audioUrl);
+		if(!this.state.playing) {
+			audio.play();
+			this.setState({
+				playing:true,
+				playingUrl: audioUrl,
+				audio
+			});
+
+		} else {
+			if(this.state.playingUrl === audioUrl) {
+				this.state.audio.pause();
+				this.setState({
+					playing: false
+				});
+			} else if(this.state.playingUrl) {
+				this.state.audio.pause();
+				audio.play();
+				this.setState({
+					playing: true,
+					playingUrl: audioUrl,
+					audio
+				});
+			}		
+		}
+	}
+
 	render() {
 		console.log('this.props',this.props);
 		const { tracks } = this.props;
@@ -11,12 +49,17 @@ class Gallery extends Component {
 					const trackImg = track.album && track.album.images ? track.album.images[0].url : null;
 					return (
 						trackImg!==null 
-						? <div key={k} className="track">					
+						? <div key={k} className="track" onClick={() => this.playAudio(track.preview_url)}>					
 							<img 
 								src={trackImg}
 								className="track-img"
 								alt="track"
 							/>
+							<div className="track-play">
+								<div className={this.state.playingUrl===track.preview_url?'track-play-inner-active':'track-play-inner'}>
+									{ this.state.playingUrl===track.preview_url ? <span>| |</span> : <span>&#9654;</span> }
+								</div>
+							</div>
 							<p className="track-text">{track.name}</p>
 						</div>
 						: <div></div>
